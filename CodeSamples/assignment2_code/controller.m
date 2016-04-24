0ebc36a85a0f7355cc216d6f84d0894ab72dd87f
dementor <soumyadeep9@gmail.com>
2016-04-25 03:42:38 +0530

    Solved Assignment 2

--- CodeSamples/assignment2_code/controller.m
+++ CodeSamples/assignment2_code/controller.m
@@ -14,9 +14,29 @@ function [ u1, u2 ] = controller(~, state, des_state, params)
 %   Using these current and desired states, you have to compute the desired
 %   controls
 
-u1 = 0;
-u2 = 0;
+Kdy = 10;
+Kpy = 0.01;
 
+Kpz = 100.0;
+Kdz = 25;
+
+Kpr = 5000.0;
+Kdr = 100;
+
+y_error = des_state.pos(1) - state.pos(1);
+ydot_error = des_state.vel(1) - state.vel(1);
+z_error = des_state.pos(2) - state.pos(2);
+zdot_error = des_state.vel(2) - state.vel(2);
+
+roll_angle = -(des_state.acc(1) + (Kdy*ydot_error) + (Kpy*y_error))/params.gravity;
+
+roll_error = roll_angle - state.rot;
+rolldot_error = -state.omega;
+rollddot = 0;
+
+u1 = params.mass*(params.gravity + des_state.acc(2) + (Kdz*zdot_error) + (Kpz*z_error));
+u2 = params.Ixx*(rollddot + (Kdr*rolldot_error) + (Kpr*roll_error));
+%u2 = 0;
 % FILL IN YOUR CODE HERE
 
 end
