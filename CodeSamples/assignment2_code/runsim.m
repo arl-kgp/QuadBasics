513c0f4c90e3165560581d100bdf6ae3acd6ac24
dementor <soumyadeep9@gmail.com>
2016-09-05 03:04:32 +0530

    Updated

--- CodeSamples/assignment2_code/runsim.m
+++ CodeSamples/assignment2_code/runsim.m
@@ -7,7 +7,7 @@ addpath('trajectories');
 controlhandle = @controller;
 
 % Choose which trajectory you want to test with
-trajhandle = @traj_line;
-% trajhandle = @traj_sine;
+%trajhandle = @traj_line;
+trajhandle = @traj_sine;
 
 [t, state] = simulation_2d(controlhandle, trajhandle);
